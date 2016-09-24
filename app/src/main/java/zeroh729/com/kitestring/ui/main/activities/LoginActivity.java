e7baac9257797e7c2bd54ed2ef934b800d671a05
vilsonluffy <vilson.espayos.lu@gmail.com>
2016-09-25 02:28:58 +0800

    Implement login

--- app/src/main/java/zeroh729/com/kitestring/ui/main/activities/LoginActivity.java
+++ app/src/main/java/zeroh729/com/kitestring/ui/main/activities/LoginActivity.java
@@ -1,10 +1,16 @@
 package zeroh729.com.kitestring.ui.main.activities;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
@@ -30,8 +36,75 @@ public class LoginActivity extends BaseActivity {
     @ViewById(R.id.tv_switch)
     TextView tv_switch;
 
+    Boolean isLogin = true;
+    FirebaseAuth.AuthStateListener mAuthListener;
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    LoginActivity_.intent(LoginActivity.this).start();
+                } else {
+
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
     @AfterViews
     public void afterviews(){
 
+        tv_switch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isLogin){
+                    isLogin = false;
+                    btn_login_register.setText("Register");
+                } else {
+                    isLogin = true;
+                    btn_login_register.setText("Login");
+                }
+
+            }
+        });
+
+        btn_login_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isLogin){
+                    loginUser();
+                } else {
+                    registerUser();
+                }
+            }
+        });
+
+
+    }
+
+    public void loginUser(){
+        mAuth.signInWithEmailAndPassword(et_email.getText().toString(), et_password.getText().toString());
+    }
+
+    public void registerUser(){
+        mAuth.createUserWithEmailAndPassword(et_email.getText().toString(), et_password.getText().toString());
     }
 }
