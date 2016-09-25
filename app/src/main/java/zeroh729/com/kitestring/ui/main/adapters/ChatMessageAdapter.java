09cdfb13d68f1e64f55ed2e729f3bbef023fdd2b
Zeroh729 <joyceplayingcp@yahoo.com>
2016-09-25 10:48:43 +0800

    merged code 2

--- app/src/main/java/zeroh729/com/kitestring/ui/main/adapters/ChatMessageAdapter.java
+++ app/src/main/java/zeroh729/com/kitestring/ui/main/adapters/ChatMessageAdapter.java
@@ -1,7 +1,9 @@
 package zeroh729.com.kitestring.ui.main.adapters;
 
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 
 import org.androidannotations.annotations.EBean;
 import org.androidannotations.annotations.RootContext;
@@ -28,5 +30,8 @@ public class ChatMessageAdapter  extends BaseAdapterRecyclerView<Message, ChatMe
         ChatMessageRow view = holder.getView();
         Message message = items.get(position);
         view.bind(message);
+        WindowManager windowManager = (WindowManager)context.getSystemService(Context.WINDOW_SERVICE);
+        int width = windowManager.getDefaultDisplay().getWidth();
+        view.setLayoutParams(new RecyclerView.LayoutParams(width, RecyclerView.LayoutParams.MATCH_PARENT));
     }
 }
--- app/src/main/java/zeroh729/com/kitestring/ui/main/fragments/FeedDetailsFragment.java
+++ app/src/main/java/zeroh729/com/kitestring/ui/main/fragments/FeedDetailsFragment.java
@@ -3,20 +3,27 @@ package zeroh729.com.kitestring.ui.main.fragments;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
 import com.melnykov.fab.FloatingActionButton;
 
 import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.ViewById;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import zeroh729.com.kitestring.Constants;
 import zeroh729.com.kitestring.R;
-import zeroh729.com.kitestring.ui.main.activities.AddFeedActivity_;
-import zeroh729.com.kitestring.ui.main.activities.FeedDetailActivity;
-import zeroh729.com.kitestring.ui.main.activities.FeedDetailActivity_;
+import zeroh729.com.kitestring.data.model.Feed;
+import zeroh729.com.kitestring.ui.main.adapters.FeedAdapter;
 
 @EFragment(R.layout.fragment_feeds)
 public class FeedDetailsFragment extends Fragment {
@@ -26,16 +33,51 @@ public class FeedDetailsFragment extends Fragment {
     @ViewById(R.id.btn_add)
     FloatingActionButton btn_add;
 
+    ArrayList<Feed> feeds = new ArrayList<>();
+
+    @Bean
+    FeedAdapter adapter;
+
     @AfterViews
     public void afterviews(){
-        rv_feeds.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter.setItems(feeds);
 
-        btn_add.setOnClickListener(new View.OnClickListener() {
+        rv_feeds.setLayoutManager(new LinearLayoutManager(getContext()));
+        rv_feeds.setAdapter(adapter);
+
+
+        FirebaseDatabase.getInstance().getReference().child(Constants.CHILD_FEEDS).addChildEventListener(new ChildEventListener() {
             @Override
-            public void onClick(View v) {
-                AddFeedActivity_.intent(getContext()).start();
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                HashMap map = (HashMap) dataSnapshot.getValue();
+                Feed feed = new Feed();
+                feed.setBlurb((String)map.get(Constants.KEY_BLURB));
+                feed.setDescription((String)map.get(Constants.KEY_DESCRIPTION));
+                feed.setTitle((String)map.get(Constants.KEY_TITLE));
+                feed.setImageUrl((String)map.get(Constants.KEY_IMAGE));
+                feeds.add(feed);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
             }
         });
-
     }
 }
